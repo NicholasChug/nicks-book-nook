af6aef670e5873bec7fbfedffd449f536e2d747d
Gabby and Nick Zagorski <nicholas.chug@gmail.com>
2020-05-18 16:39:09 -0400

    first commit lyfe

---
+++ src/pages/2020-04-01-april-fools/index.md
@@ -0,0 +1,15 @@
+---
+path: '/april-fools-2020'
+date: '2020-04-01'
+title: 'April Fools'
+author: 'Nicholas Chug'
+description: 'April Fools Sucker!'
+---
+
+Here is my main content
+It is very interesting.
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos
+aut consequuntur aspernatur corrupti ratione, odit similique tenetur
+accusantium, est nostrum esse minus iure voluptatum nihil cumque
+blanditiis non? Odit.
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layouts'
 
-const Home = () => (
+
+const About = () => (
     <Layout>
-        <h1>Home</h1>
+        <h1>About</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pulvinar convallis lorem non semper. Proin pulvinar lacus a dignissim lacinia. Morbi quis pharetra felis. Quisque id tortor orci. Donec pellentesque mauris ante, nec commodo ligula rutrum in. Suspendisse potenti. Duis sem dui, auctor et rutrum non, efficitur a lectus. Nullam tincidunt sit amet nisl condimentum dapibus.</p>
     </Layout>
 );
 
-export default Home;
+export default About;
